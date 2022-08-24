4c21c1cbf0bfaec61a98fb2e1461e64527b936b6
Danny_D <tip505@mail.ru>
2022-08-24 08:10:47 +0300

    create output array

--- Program.cs
+++ Program.cs
@@ -21,3 +21,18 @@ for (int i = 0; i < 4; i++)
         n++;
     }
 }
+
+
+string[] ArrayNew = new string[n];
+
+int j = 0;
+for (int i = 0; i < 4; i++)
+{
+    if (array[i].Length < 4)
+    {
+        ArrayNew[j] = array[i];
+        j++;
+    }
+}
+
+Console.Write("[" + string.Join(", ", ArrayNew) + "]");
