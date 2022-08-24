9b8a3eebc679b41f32d1d1dc72a3db7d98c15f79
Danny_D <tip505@mail.ru>
2022-08-24 08:07:44 +0300

    create new array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 ﻿//Контрольная работа
+
+//Первый шаг создаём массив данных с типом строка
+Console.WriteLine("Введите элементы массива типа string");
+string[] array = new string[4];
+for (int i = 0; i < 4; i++)
+{
+    array[i] = Console.ReadLine();
+}
+
+Console.Write("[" + string.Join(", ", array) + "]");
+
